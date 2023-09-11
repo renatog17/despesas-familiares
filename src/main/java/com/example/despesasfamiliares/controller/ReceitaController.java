d99d0dbc58a6dcbdaf33c3b0646f3e39e9d4d6cb
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-09-11 09:19:23 -0300

    correcoes post receita

--- src/main/java/com/example/despesasfamiliares/controller/ReceitaController.java
+++ src/main/java/com/example/despesasfamiliares/controller/ReceitaController.java
@@ -1,5 +1,6 @@
 package com.example.despesasfamiliares.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.despesasfamiliares.controller.dto.CreateReceitaDTO;
 import com.example.despesasfamiliares.controller.exceptionhandler.RegistroDuplicadoException;
 import com.example.despesasfamiliares.domain.Receita;
 import com.example.despesasfamiliares.repositorie.ReceitaRepository;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
@@ -25,7 +28,8 @@ public class ReceitaController {
 	ReceitaRepository receitaRepository;
 	
 	@PostMapping
-	public ResponseEntity<?> postReceita(@RequestBody @Valid CreateReceitaDTO newReceita, BindingResult bindingResult) throws RegistroDuplicadoException{
+	@Transactional
+	public ResponseEntity<?> postReceita(@RequestBody @Valid CreateReceitaDTO newReceita, UriComponentsBuilder uriBuilder) throws RegistroDuplicadoException{
 		Receita receita = new Receita(newReceita);
 		List<Receita> receitasByDescricao = receitaRepository.findByDescricao(receita.getDescricao());
 		for (Receita r: receitasByDescricao) {
@@ -35,6 +39,7 @@ public class ReceitaController {
 					throw new RegistroDuplicadoException("receita já foi criada para este mês");
 		}
 		receitaRepository.save(receita);
-		return null;
+		URI uri = uriBuilder.path("/receitas/{id}").buildAndExpand(receita.getId()).toUri();
+		return ResponseEntity.created(uri).body(receita);
 	}
 }
